3a146310ba9e015972ab81782a25a2df938b10eb
Daniel <danielbc@hotmail.co.uk>
2020-02-16 09:55:42 +0000

    First page

--- src/Sessions.js
+++ src/Sessions.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.css';
 import { Container } from 'react-bootstrap';
-
+import axios from 'axios';
 class Sessions extends React.Component {
 
   Sessions() {
-
+    this.state = { }
+    setTimeout(() => {
+      axios.get("https://stg-api.monotron.me/frontend/clients").then(resp => {
+        this.setState({data: resp});
+      })
+    }, 100);
   }
 
 
   render() {
+    console.log(this.state);
     return (
       <Container>
         <h1>Home</h1>
--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Sessions from './Sessions';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 
-ReactDOM.render(<Sessions />, document.getElementById('root'));
+const routing = (
+    <Router>
+      <div>
+        <Route exact path="/" component={Sessions} />
+        <Route path="/session/:sid" component={GamePanel} />
+      </div>
+    </Router>
+  )
+ReactDOM.render(routing, document.getElementById('root'))
