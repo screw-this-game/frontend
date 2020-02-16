cedb96e372cfcd7c472fc2fdfc55341dfb8f214a
Daniel <danielbc@hotmail.co.uk>
2020-02-16 09:39:17 +0000

    Testing

---
+++ src/Sessions.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Container } from 'react-bootstrap';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Container>
+        <h1>Home</h1>
+      </Container>
+    )
+  }
+}
+
+export default App;
